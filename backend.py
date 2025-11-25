5e134f01b5a17525a305735fe0a7203ee114c341
amelia89042 <amelia@aosullivan.co.nz>
2025-11-25 15:06:04 +1300

    revert backend to working w/out app

--- backend.py
+++ backend.py
@@ -7,37 +7,23 @@ from pathlib import Path
 from MathcadPy import Mathcad
 
 
-app = Flask(__name__)
-
 # create an instance of the Mathcad class - this object represents the Mathcad application
 mathcad_app = Mathcad(visible = False)
 
 #open worksheet from path
 mathcad_worksheet = mathcad_app.open(Path(r"C:\Users\Amelia\Documents\Calculation-Automation\s_c.mcdx"))
 
-#define API
-@app.route('/calculate-sc', methods = ['POST'])
-def calculate_sc():
-    data = request.json
 
-    W = float(data['W'])
-    L = float(data['L'])
 
-    #set inputs
-    mathcad_worksheet.set_real_input("w", W)
-    mathcad_worksheet.set_real_input("L", L)
+mathcad_worksheet.set_real_input("W", 0.7)
+mathcad_worksheet.set_real_input("L", 3.6)
 
-    #recalculate
-    mathcad_worksheet.calculate()
+s_c, uints, err = mathcad_worksheet._get_output("s_c")
 
-    # retrieve output
-    s_c_val, units, err = mathcad_worksheet._get_output("s_c")
+print(f"s_c is {s_c:.2f}")
+# Close the worksheet (optional, you can also save)
+mathcad_worksheet.close(save_option="Save")
 
-    return jsonify({
-        's_c': s_c_val,
-        'units':unis,
-        'error':err
-    })
+# Quit Mathcad application
+mathcad_app.quit(save_option="Save")
 
-if __name__ == '__main__':
-    app.run(debug = True)
