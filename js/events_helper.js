50abcadc7d0fe839b336264fd9ae099f7d0f4757
amelia89042 <amelia@aosullivan.co.nz>
2025-12-16 10:49:55 +1300

    debug the rd swithc

--- js/events_helper.js
+++ js/events_helper.js
@@ -368,21 +368,29 @@ export function runCalculations(){
     // NO GEOGRID THICKNESS
     //-----------------------
 
-
-    // Switch Rd_subgrade for cohesive and granular
-    if (getSoilType() == "cohesive"){
-       document.getElementsByClassName("Rd1_subgradeSWITCH")
-        = "c<sub>u</sub>N<sub>c</sub>s<sub>c1</sub>";
-        document.getElementsByClassName("Rd2_subgradeSWITCH")
-        = " 0.5γ<sub>s</sub>W<sub>d</sub>N<sub>γs</sub>s<sub>γ1</sub>";
-    } else if (soilType() == "granular"){
-       document.getElementsByClassName("Rd1_subgradeSWITCH")
-        = "c<sub>u</sub>N<sub>c</sub>s<sub>c1</sub>";
-        document.getElementsByClassName("Rd2_subgradeSWITCH")
-        = " 0.5γ<sub>s</sub>W<sub>d</sub>N<sub>γs</sub>s<sub>γ2</sub>";
-    }
-
     showElement("no-geogrid-thickness-box");
+        
+    // Switch Rd_subgrade for cohesive and granular
+    if (getSoilType() === "cohesive") {
+
+        document.querySelectorAll(".Rd1_subgradeSWITCH").forEach(el => {
+            el.innerHTML = "c<sub>u</sub>N<sub>c</sub>s<sub>c1</sub>";
+        });
+
+        document.querySelectorAll(".Rd2_subgradeSWITCH").forEach(el => {
+            el.innerHTML = "0.5γ<sub>s</sub>W<sub>d</sub>N<sub>γs</sub>s<sub>γ1</sub>";
+        });
+
+    } else if (getSoilType() === "granular") {
+
+        document.querySelectorAll(".Rd1_subgradeSWITCH").forEach(el => {
+            el.innerHTML = "c<sub>u</sub>N<sub>c</sub>s<sub>c1</sub>";
+        });
+
+        document.querySelectorAll(".Rd2_subgradeSWITCH").forEach(el => {
+            el.innerHTML = "0.5γ<sub>s</sub>W<sub>d</sub>N<sub>γs</sub>s<sub>γ2</sub>";
+        });
+    }
 
     
     loadCalculated("D1NoGeogrid", DNoGeogridF(W, q1dB, Rd1_subgrade, gamma_platform, kptandelta, sp1));
